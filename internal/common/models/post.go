c029935b9f4d3132f889b4a478dc4bb639a44322
Matthew Dawber <matt@dawbs.co.uk>
2023-09-19 21:09:57 +0100

    refactor: Remove internal directory

--- internal/common/models/post.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type Post struct {
-    gorm.Model  // adds ID, created_at etc.
-    Title       string `json:"title"`
-	Content		string `json:"content"`
-}
